b903754068e8b236d4d31491629e628efad57666
mae616 <50073576+mae616@users.noreply.github.com>
2022-04-21 20:40:53 +0900

    Initial commit

---
+++ tests/02.test.js
@@ -0,0 +1,23 @@
+/**
+ * 配列から削除する
+ *
+ * 配列aの最後の要素を削除した新しい配列resultを作ってください。
+ * 削除した最後の要素はdeletedItemに入れてください。
+ * ただし、配列aを変更してはいけません。
+ */
+
+// 元データ
+const a = [1, 2, 3, 4];
+
+// この下にコードを書く
+const result = [];
+const deletedItem = 0;
+
+// ここまで。この下は編集しない
+
+// テストコード
+test("02", () => {
+  expect(a).toEqual([1, 2, 3, 4]);
+  expect(result).toEqual([1, 2, 3]);
+  expect(deletedItem).toBe(4);
+});
